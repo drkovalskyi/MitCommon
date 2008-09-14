b9dd555b08bf1e89d46b98f3ea8e6df996ff0740
loizides <>
2008-09-14 14:07:18 +0000

    Have Double32 types as well

--- DataFormats/interface/Types.h
+++ DataFormats/interface/Types.h
@@ -1,9 +1,10 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: Types.h,v 1.1 2008/09/04 17:10:54 loizides Exp $
+// $Id: Types.h,v 1.2 2008/09/10 03:27:34 loizides Exp $
 //
 // Types
 //
-// Here we define common types.
+// Here we define common types. For help on the GenVector lib, see
+// http://root.cern.ch/root/html/MATH_GENVECTOR_Index.html
 //
 // Authors: C.Loizides
 //--------------------------------------------------------------------------------------------------
@@ -12,23 +13,41 @@
 #define MITCOMMON_DATAFORMATS_TYPES_H
  
 #include <Rtypes.h>
-#include <Math/Point3Dfwd.h>
+#include <Math/Vector3D.h>
+#include <Math/Vector4D.h>
+#include <Math/Vector3Dfwd.h>
+#include <Math/Vector4Dfwd.h>
 #include <Math/GenVector/LorentzVector.h>
 #include <Math/SMatrix.h>
 
 namespace mithep
 {
-  typedef ROOT::Math::LorentzVector< ::ROOT::Math::PxPyPzE4D<double> > FourVector;
-//typedef ::ROOT::Math::LorentzVector< ::ROOT::Math::PtEtaPhiE4D<double> > FourVectorEtaPhi;
-  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<double>,
+  typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<Double_t> > FourVector;
+  typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<Double_t> > FourVectorM;
+  typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<Double_t> > FourVectorE;
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<Double_t>,
                                              ROOT::Math::DefaultCoordinateSystemTag> 
                                              ThreeVector;
-  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::CylindricalEta3D<double>,
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::CylindricalEta3D<Double_t>,
                                              ROOT::Math::DefaultCoordinateSystemTag> 
-                                             ThreeVectorEtaPhi;
-  typedef ROOT::Math::SMatrix<double,3,3,ROOT::Math::MatRepSym<double,3> >   ThreeSymMatrix;
-  typedef ROOT::Math::SMatrix<double,7,7,ROOT::Math::MatRepSym<double,7> >   SevenSymMatrix;
-  typedef ROOT::Math::SMatrix<double,3,3,ROOT::Math::MatRepStd<double,3,3> > ThreeMatrix;
-  typedef ROOT::Math::SMatrix<double,7,7,ROOT::Math::MatRepStd<double,7,7> > SevenMatrix;
+                                             ThreeVectorC;
+  typedef ROOT::Math::SMatrix<Double_t,3,3,ROOT::Math::MatRepSym<Double_t,3> >   ThreeSymMatrix;
+  typedef ROOT::Math::SMatrix<Double_t,7,7,ROOT::Math::MatRepSym<Double_t,7> >   SevenSymMatrix;
+  typedef ROOT::Math::SMatrix<Double_t,3,3,ROOT::Math::MatRepStd<Double_t,3,3> >    ThreeMatrix;
+  typedef ROOT::Math::SMatrix<Double_t,7,7,ROOT::Math::MatRepStd<Double_t,7,7> >    SevenMatrix;
+
+  typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<Double32_t> > FourVector32;
+  typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<Double32_t> > FourVectorM32;
+  typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<Double32_t> > FourVectorE32;
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<Double32_t>,
+                                             ROOT::Math::DefaultCoordinateSystemTag> 
+                                             ThreeVector32;
+  typedef ROOT::Math::DisplacementVector3D<ROOT::Math::CylindricalEta3D<Double32_t>,
+                                             ROOT::Math::DefaultCoordinateSystemTag> 
+                                             ThreeVectorC32;
+  typedef ROOT::Math::SMatrix<Double32_t,3,3,ROOT::Math::MatRepSym<Double32_t,3> > ThreeSymMatrix32;
+  typedef ROOT::Math::SMatrix<Double32_t,7,7,ROOT::Math::MatRepSym<Double32_t,7> > SevenSymMatrix32;
+  typedef ROOT::Math::SMatrix<Double32_t,3,3,ROOT::Math::MatRepStd<Double32_t,3,3> >  ThreeMatrix32;
+  typedef ROOT::Math::SMatrix<Double32_t,7,7,ROOT::Math::MatRepStd<Double32_t,7,7> >  SevenMatrix32;
 }
 #endif
