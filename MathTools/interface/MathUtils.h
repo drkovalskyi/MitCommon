8e251fd57567b57269c9120e0668af9a6b73a029
loizides <>
2009-02-18 15:38:26 +0000

    Added FourVectorM functions.

--- MathTools/interface/MathUtils.h
+++ MathTools/interface/MathUtils.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: MathUtils.h,v 1.3 2008/09/10 03:27:34 loizides Exp $
+// $Id: MathUtils.h,v 1.4 2008/11/24 14:23:00 loizides Exp $
 //
 // MathUtils
 //
@@ -21,8 +21,10 @@ namespace mithep
       static Double_t AddInQuadrature(Double_t a, Double_t b);  
       static Double_t DeltaPhi(Double_t phi1, Double_t phi2);
       static Double_t DeltaPhi(const FourVector &v1, const FourVector &v2);
+      static Double_t DeltaPhi(const FourVectorM &v1, const FourVectorM &v2);
       static Double_t DeltaR(Double_t phi1, Double_t eta1, Double_t phi2, Double_t eta2);
       static Double_t DeltaR(const FourVector &v1, const FourVector &v2);
+      static Double_t DeltaR(const FourVectorM &v1, const FourVectorM &v2);
       static Double_t Eta2Theta(Double_t eta);
       static Double_t Theta2Eta(Double_t theta);
   };
--- MathTools/src/MathUtils.cc
+++ MathTools/src/MathUtils.cc
@@ -1,4 +1,4 @@
-// $Id: MathUtils.cc,v 1.3 2008/09/10 03:27:35 loizides Exp $
+// $Id: MathUtils.cc,v 1.4 2008/11/24 14:23:01 loizides Exp $
 
 #include "MitCommon/MathTools/interface/MathUtils.h"
 
@@ -7,12 +7,16 @@ using namespace mithep;
 //--------------------------------------------------------------------------------------------------
 Double_t MathUtils::AddInQuadrature(Double_t a, Double_t b)
 {
+  // Add quantities in quadrature.
+
   return(TMath::Sqrt(a*a + b*b));
 }
 
 //--------------------------------------------------------------------------------------------------
 Double_t MathUtils::DeltaPhi(Double_t phi1, Double_t phi2)
 {
+  // Compute DeltaPhi between two given angles. Results is in [-pi/2,pi/2].
+
   Double_t dphi = TMath::Abs(phi1-phi2);
   while (dphi>TMath::Pi())
     dphi = TMath::Abs(dphi - TMath::TwoPi());
@@ -22,12 +26,24 @@ Double_t MathUtils::DeltaPhi(Double_t phi1, Double_t phi2)
 //--------------------------------------------------------------------------------------------------
 Double_t MathUtils::DeltaPhi(const FourVector &v1, const FourVector &v2)
 {
+  // Compute DeltaPhi between two given angles. Results is in [-pi/2,pi/2].
+
+  return DeltaPhi(v1.Phi(),v2.Phi());
+}
+
+//--------------------------------------------------------------------------------------------------
+Double_t MathUtils::DeltaPhi(const FourVectorM &v1, const FourVectorM &v2)
+{
+  // Compute DeltaPhi between two given angles. Results is in [-pi/2,pi/2].
+
   return DeltaPhi(v1.Phi(),v2.Phi());
 }
 
 //--------------------------------------------------------------------------------------------------
 Double_t MathUtils::DeltaR(Double_t phi1, Double_t eta1, Double_t phi2, Double_t eta2)
 {
+  // Compute DeltaR between two given points in the eta/phi plane.
+
   Double_t dphi = DeltaPhi(phi1, phi2);
   Double_t deta = eta1-eta2;
   Double_t dR = TMath::Sqrt(dphi*dphi + deta*deta);
@@ -37,17 +53,31 @@ Double_t MathUtils::DeltaR(Double_t phi1, Double_t eta1, Double_t phi2, Double_t
 //--------------------------------------------------------------------------------------------------
 Double_t MathUtils::DeltaR(const FourVector &v1, const FourVector &v2)
 {
+  // Compute DeltaR between two given points in the eta/phi plane.
+
+  return MathUtils::DeltaR(v1.Phi(),v1.Eta(),v2.Phi(),v2.Eta());
+}
+
+//--------------------------------------------------------------------------------------------------
+Double_t MathUtils::DeltaR(const FourVectorM &v1, const FourVectorM &v2)
+{
+  // Compute DeltaR between two given points in the eta/phi plane.
+
   return MathUtils::DeltaR(v1.Phi(),v1.Eta(),v2.Phi(),v2.Eta());
 }
 
 //--------------------------------------------------------------------------------------------------
 Double_t MathUtils::Eta2Theta(Double_t eta) 
 { 
+  // Compute theta from given eta value.
+
   return 2.*TMath::ATan(exp(-eta)); 
 }
 
 //--------------------------------------------------------------------------------------------------
 Double_t MathUtils::Theta2Eta(Double_t theta) 
 { 
+  // Compute eta from given theta value.
+
   return -TMath::Log(TMath::Tan(theta/2.)); 
 }
