c486628418d403f2ac8b88572e01acf3ad095b04
loizides <>
2009-08-13 09:50:40 +0000

    Added pointer based Delta function templates.

--- MathTools/interface/MathUtils.h
+++ MathTools/interface/MathUtils.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: MathUtils.h,v 1.9 2009/06/24 14:51:04 loizides Exp $
+// $Id: MathUtils.h,v 1.10 2009/07/20 03:12:22 loizides Exp $
 //
 // MathUtils
 //
@@ -24,12 +24,18 @@ namespace mithep
       static Double_t DeltaPhi(Double_t phi1, Double_t phi2);
       template<class V1, class V2> 
       static Double_t DeltaPhi(const V1 &v1, const V2 &v2);
+      template<class V1, class V2> 
+      static Double_t DeltaPhi(const V1 *v1, const V2 *v2);
       static Double_t DeltaR(Double_t phi1, Double_t eta1, Double_t phi2, Double_t eta2);
+      template<class V1, class V2> 
+      static Double_t DeltaR(const V1 *v1, const V2 *v2);
       template<class V1, class V2> 
       static Double_t DeltaR(const V1 &v1, const V2 &v2);
       static Double_t DeltaR2(Double_t phi1, Double_t eta1, Double_t phi2, Double_t eta2);
       template<class V1, class V2> 
       static Double_t DeltaR2(const V1 &v1, const V2 &v2);
+      template<class V1, class V2> 
+      static Double_t DeltaR2(const V1 *v1, const V2 *v2);
       static Double_t Eta2Theta(Double_t eta);
       static Double_t Theta2Eta(Double_t theta);
 
@@ -41,26 +47,53 @@ namespace mithep
 template<class V1, class V2>
 Double_t mithep::MathUtils::DeltaPhi(const V1 &v1, const V2 &v2)
 {
-  // DeltaPhi between two given objects
+  // DeltaPhi between two given objects.
 
   return mithep::MathUtils::DeltaPhi(v1.Phi(),v2.Phi());
 }
 
 //--------------------------------------------------------------------------------------------------
 template<class V1, class V2>
+Double_t mithep::MathUtils::DeltaPhi(const V1 *v1, const V2 *v2)
+{
+  // DeltaPhi between two given objects.
+
+  return mithep::MathUtils::DeltaPhi(v1->Phi(),v2->Phi());
+}
+
+//--------------------------------------------------------------------------------------------------
+template<class V1, class V2>
 Double_t mithep::MathUtils::DeltaR(const V1 &v1, const V2 &v2)
 {
-  // DeltaR between two given objects
+  // DeltaR between two given objects.
 
   return mithep::MathUtils::DeltaR(v1.Phi(),v1.Eta(),v2.Phi(),v2.Eta());
 }
 
 //--------------------------------------------------------------------------------------------------
 template<class V1, class V2>
-Double_t mithep::MathUtils::DeltaR2(const V1 &v1, const V2 &v2)
+Double_t mithep::MathUtils::DeltaR(const V1 *v1, const V2 *v2)
 {
   // DeltaR between two given objects
 
+  return mithep::MathUtils::DeltaR(v1->Phi(),v1->Eta(),v2->Phi(),v2->Eta());
+}
+
+//--------------------------------------------------------------------------------------------------
+template<class V1, class V2>
+Double_t mithep::MathUtils::DeltaR2(const V1 &v1, const V2 &v2)
+{
+  // DeltaR between two given objects.
+
   return mithep::MathUtils::DeltaR2(v1.Phi(),v1.Eta(),v2.Phi(),v2.Eta());
+}
+
+//--------------------------------------------------------------------------------------------------
+template<class V1, class V2>
+Double_t mithep::MathUtils::DeltaR2(const V1 *v1, const V2 *v2)
+{
+  // DeltaR between two given objects.
+
+  return mithep::MathUtils::DeltaR2(v1->Phi(),v1->Eta(),v2->Phi(),v2->Eta());
 }
 #endif
