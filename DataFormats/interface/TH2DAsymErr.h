c011b4074a6d134e9d056b305933d6e63831ce83
sixie <>
2010-03-27 13:18:42 +0000

    Add some accessors to get bin content by bin number

--- DataFormats/interface/TH2DAsymErr.h
+++ DataFormats/interface/TH2DAsymErr.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: TH2DAsymErr.h,v 1.2 2009/08/11 09:02:24 loizides Exp $
+// $Id: TH2DAsymErr.h,v 1.3 2009/10/21 15:04:06 sixie Exp $
 //
 // TH2DAsymErr
 //
@@ -34,18 +34,24 @@ namespace mithep
       ~TH2DAsymErr() {}
 
       using TH2D::GetBinContent;
-      Double_t      GetError(Double_t x, Double_t y, EErrType t);
-      Double_t      GetStatErrorLow(Double_t x, Double_t y)   { return GetError(x,y,kStatErrLow);  }
-      Double_t      GetStatErrorHigh(Double_t x, Double_t y)  { return GetError(x,y,kStatErrHigh); }
-      Double_t      GetSysErrorLow(Double_t x, Double_t y)    { return GetError(x,y,kSysErrLow);   }
-      Double_t      GetSysErrorHigh(Double_t x, Double_t y)   { return GetError(x,y,kSysErrHigh);  }
+      Double_t  GetError(Double_t x, Double_t y, EErrType t);
+      Double_t  GetBinError(Int_t b, Int_t c, EErrType t);
+      Double_t  GetStatErrorLow(Double_t x, Double_t y)  { return GetError(x,y,kStatErrLow);     }
+      Double_t  GetBinStatErrorLow(Int_t b, Int_t c)     { return GetBinError(b,c,kStatErrLow);  }
+      Double_t  GetStatErrorHigh(Double_t x, Double_t y) { return GetError(x,y,kStatErrHigh);    }
+      Double_t  GetBinStatErrorHigh(Int_t b, Int_t c)    { return GetBinError(b,c,kStatErrHigh); }
+      Double_t  GetSysErrorLow(Double_t x, Double_t y)   { return GetError(x,y,kSysErrLow);      }
+      Double_t  GetBinSysErrorLow(Int_t b, Int_t c)      { return GetBinError(b,c,kSysErrLow);   }
+      Double_t  GetSysErrorHigh(Double_t x, Double_t y)  { return GetError(x,y,kSysErrHigh);     }
+      Double_t  GetBinSysErrorHigh(Int_t b, Int_t c)     { return GetBinError(b,c,kSysErrHigh);  }
       using TH2D::SetBinContent;
-      void          SetBinContent(Int_t binx, Int_t biny, Double_t value, 
-                                  Double_t statErrorLow, Double_t statErrorHigh,
-                                  Double_t sysErrorLow, Double_t sysErrorHigh);
-      void          SetBinError(Int_t binx, Int_t biny,
-                                Double_t statErrorLow, Double_t statErrorHigh, 
-                                Double_t sysErrorLow, Double_t sysErrorHigh);
+      void      SetBinContent(Int_t binx, Int_t biny, Double_t value, 
+                              Double_t statErrorLow, Double_t statErrorHigh,
+                              Double_t sysErrorLow, Double_t sysErrorHigh);
+      using TH2D::SetBinError;
+      void      SetBinError(Int_t binx, Int_t biny,
+                            Double_t statErrorLow, Double_t statErrorHigh, 
+                            Double_t sysErrorLow, Double_t sysErrorHigh);
 
     protected:
 
