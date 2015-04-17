f0e7bc010211d1699e6eb28528e11810d373fed8
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-17 15:39:10 -0400

    little tweaks

--- bin/genDict.sh
+++ bin/genDict.sh
@@ -8,6 +8,9 @@ fi
 
 PACKAGES="$@"
 
+echo "Generating ROOT dictionaries for:"
+echo " $PACKAGES"
+
 for PACKAGE in $PACKAGES
 do
   DICTDIR=$CMSSW_BASE/src/$PACKAGE/dict
@@ -22,18 +25,27 @@ do
   for LINKDEF in $(ls $DICTDIR/*LinkDef.h); do
     # collect include headers from LinkDef file
     HEADERS=$(sed -n 's|#include *"\([^"]*\)"|'$CMSSW_BASE'/src/\1|p' $LINKDEF | tr '\n' ' ')
-    # the name of the dictionary code file generated from ABCLinkDef.h will be ABCDict.cc
-    OUTPUT=$(sed 's/LinkDef.h/Dict/' <<< $(basename $LINKDEF))
+    # the name of the dictionary code file generated from ABCLinkDef.h will be ABC_LinkDefDict.cc
+    OUTPUT=$(sed 's/LinkDef.h/_LinkDefDict/' <<< $(basename $LINKDEF))
+
+    echo rootcling -f $OUTPUT.cc -I$CMSSW_BASE/src $HEADERS $LINKDEF
     rootcling -f $OUTPUT.cc -I$CMSSW_BASE/src $HEADERS $LINKDEF
+
+    echo mv ${OUTPUT}_rdict.pcm $LIBDIR/
     mv ${OUTPUT}_rdict.pcm $LIBDIR/
   done
   
   if [ -e $DICTDIR/classes_def.xml ]
   then
-    OUTPUT=$(sed 's|\([^/]*\)/\(.*\)|\1\2|' <<< $PACKAGE)ReflexDict
+    # the name of the dictionary code file generated from classes_xml will be PACKAGE_ReflexDict.cc
+    OUTPUT=$(sed 's|\([^/]*\)/\(.*\)|\1\2|' <<< $PACKAGE)_ReflexDict
+
+    echo genreflex $DICTDIR/classes.h -s $DICTDIR/classes_def.xml -o $OUTPUT.cc --rootmap=$OUTPUT.rootmap -I$CMSSW_BASE/src
     genreflex $DICTDIR/classes.h -s $DICTDIR/classes_def.xml -o $OUTPUT.cc --rootmap=$OUTPUT.rootmap -I$CMSSW_BASE/src
 
+    echo mv $OUTPUT.rootmap $LIBDIR/
     mv $OUTPUT.rootmap $LIBDIR/
+    echo mv ${OUTPUT}_rdict.pcm $LIBDIR/
     mv ${OUTPUT}_rdict.pcm $LIBDIR/
   fi
 done
--- bin/setup.sh
+++ bin/setup.sh
@@ -5,5 +5,9 @@
 #                                                                   Apr 17, 2015 - V1 Y. Iiyama
 #----------------------------------------------------------------------------------------------
 
+echo "************************"
+echo " MitCommon/bin/setup.sh"
+echo "************************"
+
 # Generate ROOT dictionaries for classes defined in this module
 $CMSSW_BASE/src/MitCommon/bin/genDict.sh MitCommon/{DataFormats,MathTools,OptIO,Utils}
