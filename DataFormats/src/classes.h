ef49e28346551ba0c35bc556b537b3c0208fa61c
Dmytro Kovalskyi <dmytro@cern.ch>
2015-07-02 13:24:02 +0200

    added proper way to define dictionaries; class version is missing, but most likely we won't really need it

---
+++ DataFormats/src/classes.h
@@ -0,0 +1,18 @@
+// include header files containing a full definition 
+// of each class or template used in classes_def.xml
+
+#include "MitCommon/DataFormats/interface/Hist1DRat.h"
+#include "MitCommon/DataFormats/interface/TH2DAsymErr.h"
+#include "MitCommon/DataFormats/interface/TH3DAsymErr.h"
+#include "MitCommon/DataFormats/interface/Types.h"
+#include "MitCommon/DataFormats/interface/Vect3.h"
+#include "MitCommon/DataFormats/interface/Vect3C.h"
+#include "MitCommon/DataFormats/interface/Vect4M.h"
+
+// dummy declarations for templated classes 
+ 
+namespace { 
+  struct dictionary {
+    
+  };
+}
